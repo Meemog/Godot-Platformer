221e68952526a75b007ebab106c2f089d277ca01
Oscar <oscmog@gmail.com>
2026-03-24 15:44:14 +0000

    Refactored for readablity and efficiency

--- Assets/Scenes/fishing_minigame.gd
+++ Assets/Scenes/fishing_minigame.gd
@@ -1,25 +1,25 @@
 extends Node2D
+## Script containing the functionality of the fishing minigame
+
+@export var delay_time: float = 3 ## Time before game closes after it ends
 
 enum Fish_State {Lost, Won, Ongoing, Idle}
-signal game_completed(won: bool)
+signal game_completed(won: bool) ## Emitted when the game is finished with a boolean of if the player won or not
 
-@export var delay_time: float = 3
+var _drain_speed: float ## Speed at which the fishing bar decreases
+var _fish_power: float ## Step at which fishing bar increases
+var _bar: Node2D ## Reference to anchor point for the internal bar
 
-var _drain_speed: float
-var _fish_power: float
-var _game_state = Fish_State.Idle
-var _bar: Node2D
-var _sprite: Node2D
-
-var _time_since_state_change = 0
+var _game_state: Fish_State = Fish_State.Idle ## Current state of the game
+var _time_since_state_change: float = 0 ## Timer for hiding the sprite at the end of the game
 
 func _ready() -> void:
     _bar = $BarAnchor
-    
     visible = false
-    
-func start(drain_speed: float = 0.005, start_position: float = 0.3, fish_power: float = 0.15):
-    if _game_state == Fish_State.Idle:
+
+## Sets attribues to default state and starts the game
+func start(drain_speed: float = 0.005, start_position: float = 0.3, fish_power: float = 0.15) -> void:
+    if _game_state == Fish_State.Idle: # Cannot restart while game is active
         _drain_speed = drain_speed
         _fish_power = fish_power
         _bar.scale.y = start_position
@@ -27,42 +27,47 @@ func start(drain_speed: float = 0.005, start_position: float = 0.3, fish_power: 
         _time_since_state_change = 0
         
         visible = true
+
+## Stops the game early if needed
+func stop() -> void:
+    pass
     
 func _process(delta: float) -> void:
     if _game_state == Fish_State.Ongoing:
-        _game_state = change_progress(-_drain_speed)
+        _game_state = change_progress(-_drain_speed) # Lower the bar fill by the drain speed
         if _game_state == Fish_State.Lost:
-            game_completed.emit(true)
-    if _game_state == Fish_State.Lost or _game_state == Fish_State.Won:
+            game_completed.emit(true) 
+    if _game_state == Fish_State.Lost or _game_state == Fish_State.Won: # Increase timer if game has just ended
         _time_since_state_change += delta
         if _time_since_state_change > delay_time:
             _game_state = Fish_State.Idle
             visible = false
-            
-func process_fish() -> void:
+
+## Increases fishing bar by the fishing power
+func process_fish_input() -> void:
     if _game_state != Fish_State.Ongoing:
         return
-    _game_state = change_progress(_fish_power)
+    _game_state = change_progress(_fish_power) # Fill the bar by the fishing power
     if _game_state == Fish_State.Won:
         game_completed.emit(false)
 
+## Changes the progress of the bar by a relative modifier
 func change_progress(modifier: float) -> Fish_State:
-    var scale: float = _bar.scale.y
+    var temp_scale: float = _bar.scale.y
     var state: Fish_State = Fish_State.Ongoing
     
-    scale += modifier
+    temp_scale += modifier
     
-    if scale < 0:
-        scale = 0
+    if temp_scale < 0:
+        temp_scale = 0
         state = Fish_State.Lost
-    if scale > 1:
-        scale = 1
+    if temp_scale > 1:
+        temp_scale = 1
         state = Fish_State.Won
     
-    _bar.scale.y = scale
+    _bar.scale.y = temp_scale # Change scale in one step
     return state
-    
 
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("fish"):
-        process_fish()
+        process_fish_input()
--- Assets/Scenes/player.gd
+++ Assets/Scenes/player.gd
@@ -1,52 +1,67 @@
 extends CharacterBody2D
+## Script determining the behaviours of the player
 
 enum Wall_State {None, Left, Right}
 
 @export_category("Horizontal Movement")
-@export var soft_max_velocity: float = 300
-@export var hard_max_velocity: float = 600
-@export var acceleration: float = 50
-@export var horizontal_dampening = 20
+@export var soft_max_velocity: float = 300 ## Horizontal velocity at which accelleration will no longer apply.
+@export var hard_max_velocity: float = 600 ## Horizontal velocity that player cannot exceed.
+@export var acceleration: float = 50 ## Step at which velocity is increased each tick while accelerating.
+@export var horizontal_dampening = 20 ## Step at which velocity is decreased each tick when not accelerating.
 
 @export_category("Vertical Movement")
-@export var gravity: float = 25
-@export var jump_force: float = 500
-@export var jump_remember_time: float = 0.15
-@export var coyote_time: float = 0.2
-@export var jump_delay: float = 0.2
-@export var cut_jump_mult: float = 0.45
+@export var gravity: float = 25 ## Step at which vertical velocity is increased each tick.
+@export var jump_force: float = 500 ## Vertical velocity player is set to on jump.
+@export var jump_buffer_time: float = 0.15 ## Time in seconds that a jump input is buffered if the player is not currently grounded when pressed.
+@export var coyote_time: float = 0.2 ## Time in seconds that the player remembers being grounded for after leaving ledge.
+@export var jump_delay: float = 0.2 ## Minimum time in seconds between jumps
+@export var cut_jump_mult: float = 0.45 ## Multiplier applied to upward velocity after jump key is released
 
 @export_category("Wall Jump")
-@export var w_jump_vector: Vector2 = Vector2(1.5, -1)
-@export var w_jump_force: float = 700
-@export var wall_slip_speed = 100
+@export var w_jump_vector: Vector2 = Vector2(1.5, -1) ## Vector descriping the direction of a wall jump
+@export var w_jump_force: float = 700 ## Multiplier that is applied to the normalized [constant w_jump_vector]
+@export var wall_slip_speed = 100 ## Vertical velocity the player moves at while moving into a wall
 
-var current_horizontal_scalar: float = 0
+var _current_horizontal_scalar: float = 0 ## Value between -1 and 1 storing current horizontal input data
 
 # Jump Timers
-var time_since_grounded: float
-var time_since_jump_input: float
-var time_since_last_jump: float
+var _time_since_grounded: float ## Time in seconds since player was grounded
+var _time_since_jump_input: float ## Time in seconds since jump input was pressed
+var _time_since_last_jump: float ## Time in seconds since player jumped
 
 # Wall Jump
-var w_jump_vector_normal: Vector2
-var wall_state: Wall_State
+var _w_jump_vector_normal: Vector2 ## Normalised wall jump direction vector
+var _wall_state: Wall_State ## Stores which side of the player is touching a wall if any
 
-var tps_adjustment: float
+# Misc Physics
+var _tps_adjustment: float ## Physics adjustment to ensure forces are calculated correctly when physics ticks are inconsistant
+
+# Children
+var _sprite: AnimatedSprite2D ## Sprite of the player
+var _sprite_pivot: Node2D ## Pivot point of the player sprite
+var _area_grounded: Area2D ## Area used to check if the player is grounded
+var _area_wall_left: Area2D ## Area used to check if the player is touching a wall to their left
+var _area_wall_right: Area2D ## Area used to check if the player is touching a wall to their left
 
 func _ready() -> void:
-    time_since_grounded = coyote_time
-    time_since_jump_input = jump_remember_time
-    time_since_last_jump = jump_delay
+    _time_since_grounded = coyote_time
+    _time_since_jump_input = jump_buffer_time
+    _time_since_last_jump = jump_delay
 
-    w_jump_vector_normal = w_jump_vector.normalized()
-    wall_state = Wall_State.None
+    _w_jump_vector_normal = w_jump_vector.normalized()
+    _wall_state = Wall_State.None
     
-    $AnimationPivot/AnimatedSprite2D.play("Idle")
+    _sprite = $AnimationPivot/AnimatedSprite2D
+    _sprite_pivot = $AnimationPivot
+    _area_grounded = $Grounded
+    _area_wall_left = $WallLeft
+    _area_wall_right = $WallRight
+    
+    _sprite.play("Idle")
 
 
 func _physics_process(delta: float) -> void:
-    tps_adjustment = Engine.physics_ticks_per_second * delta
+    _tps_adjustment = Engine.physics_ticks_per_second * delta
     
     process_horizontal()
     
@@ -58,22 +73,23 @@ func _physics_process(delta: float) -> void:
     
     move_and_slide()
 
-func _process(delta: float) -> void:
-    if velocity.x == 0 or time_since_grounded > 0.2:
-        $AnimationPivot/AnimatedSprite2D.play("Idle")
+func _process(_delta: float) -> void:
+    if velocity.x == 0 or _time_since_grounded > 0.2:
+        _sprite.play("Idle")
     else:
-        $AnimationPivot/AnimatedSprite2D.play("Walk")
+        _sprite.play("Walk")
         
-    if current_horizontal_scalar == 1:
-        $AnimationPivot.scale.x = 1
-    elif current_horizontal_scalar == -1:
-        $AnimationPivot.scale.x = -1
-
+    if _current_horizontal_scalar == 1:
+        _sprite_pivot.scale.x = 1
+    elif _current_horizontal_scalar == -1:
+        _sprite_pivot.scale.x = -1
+        
+## Called on each [method Node._physics_process] and determines what the horizontal velocity of the player should be
 func process_horizontal() -> void:
     var horizontal_scalar = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-    current_horizontal_scalar = horizontal_scalar
+    _current_horizontal_scalar = horizontal_scalar
     
-    var sign = velocity.x / abs(velocity.x)
+    var direction = velocity.x / abs(velocity.x)
     
     # Stop acceleration past soft speed cap
     if horizontal_scalar == sign and abs(velocity.x) > soft_max_velocity:
@@ -81,63 +97,67 @@ func process_horizontal() -> void:
     
     # If a movememnt key is pressed, apply acceleration
     if horizontal_scalar != 0:
-        velocity.x += horizontal_scalar * acceleration * tps_adjustment
+        velocity.x += horizontal_scalar * acceleration * _tps_adjustment
     # Otherwise apply dampening
     elif velocity.x != 0:
         var current_vel = abs(velocity.x)
-        current_vel -= horizontal_dampening * tps_adjustment
+        current_vel -= horizontal_dampening * _tps_adjustment
         if current_vel < 0:
             current_vel = 0
         velocity.x = current_vel * sign
     
     # Prevent velocity from exceeding max velocity
     if abs(velocity.x) > hard_max_velocity:
-        velocity.x = hard_max_velocity * sign
+        velocity.x = hard_max_velocity * direction
     
-
+## Called on each [method Node._physics_process] and determines what walls the player is currently on if any
 func process_walls():
     # Doesn't account for if the player is on 2 walls at once
-    if ($WallLeft.get_overlapping_bodies().size() > 0):
-        wall_state = Wall_State.Left
-    elif ($WallRight.get_overlapping_bodies().size() > 0):
-        wall_state = Wall_State.Right
+    if (_area_wall_left.get_overlapping_bodies().size() > 0):
+        _wall_state = Wall_State.Left
+    elif (_area_wall_right.get_overlapping_bodies().size() > 0):
+        _wall_state = Wall_State.Right
     else:
-        wall_state = Wall_State.None
+        _wall_state = Wall_State.None
 
+## Called on each [method Node._physics_process] and checks if the player should be jumping this physics tick
 func process_jump(delta: float) -> void:
-    time_since_grounded += delta
-    time_since_jump_input += delta
-    time_since_last_jump += delta
+    _time_since_grounded += delta
+    _time_since_jump_input += delta
+    _time_since_last_jump += delta
     
-    if $Grounded.get_overlapping_bodies().size() > 0:
-        time_since_grounded = 0
+    if _area_grounded.get_overlapping_bodies().size() > 0:
+        _time_since_grounded = 0
     
-    if time_since_jump_input < jump_remember_time and time_since_last_jump > jump_delay:
-        if time_since_grounded < coyote_time:
+    if _time_since_jump_input < jump_buffer_time and _time_since_last_jump > jump_delay:
+        if _time_since_grounded < coyote_time:
             jump()
-        elif wall_state != Wall_State.None:
+        elif _wall_state != Wall_State.None:
             wall_jump()
-        
+
+## Causes the player to jump
 func jump() -> void:
     velocity.y = -jump_force
-    time_since_last_jump = 0
+    _time_since_last_jump = 0
 
+## Causes the player to wall jump
 func wall_jump() -> void:
-    velocity = w_jump_force * w_jump_vector_normal
-    if wall_state == Wall_State.Right:
+    velocity = w_jump_force * _w_jump_vector_normal
+    if _wall_state == Wall_State.Right:
         velocity.x *= -1
         
-    time_since_last_jump = 0
-
+    _time_since_last_jump = 0
+    
+## Called on each [method Node._physics_process] and applies acceleration via gravity if applicable
 func process_gravity() -> void:
-    if wall_state != Wall_State.None and velocity.y > wall_slip_speed and current_horizontal_scalar != 0:
+    if _wall_state != Wall_State.None and velocity.y > wall_slip_speed and _current_horizontal_scalar != 0:
         velocity.y = wall_slip_speed
     else:
-        velocity.y += gravity * tps_adjustment
+        velocity.y += gravity * _tps_adjustment
 
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("jump"):
-        time_since_jump_input = 0
+        _time_since_jump_input = 0
     if event.is_action_released("jump"):
         if velocity.y < 0:
             velocity.y *= cut_jump_mult
